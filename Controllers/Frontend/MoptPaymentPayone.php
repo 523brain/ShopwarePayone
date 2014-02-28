602783bd46286053e12de6716e052dc974e2f903
Lennart Diedrich <lenn@rt-diedrich.de>
2014-02-28 16:05:31 +0100

    Transmit no vat for articles to payone when the order is tax free
    Squashed by @samuelvogel with "Fix wrong computation of tax free flag" from @stefanheppenheimer

--- Controllers/Frontend/MoptPaymentPayone.php
+++ Controllers/Frontend/MoptPaymentPayone.php
@@ -547,7 +547,7 @@ class Shopware_Controllers_Frontend_MoptPaymentPayone extends Shopware_Controlle
 
     if ($config['submitBasket'] || $clearingType === 'fnc')
     {
-      $request->setInvoicing($paramBuilder->getInvoicing($this->getBasket(), $this->getShipment()));
+      $request->setInvoicing($paramBuilder->getInvoicing($this->getBasket(), $this->getShipment(), $this->getUserData()));
     }
 
     if ($payment)
