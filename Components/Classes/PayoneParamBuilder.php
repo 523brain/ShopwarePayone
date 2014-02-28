c4e83b85693ea117bde1899b96743453c7f2d9c0
Lennart Diedrich <lenn@rt-diedrich.de>
2014-02-28 16:05:31 +0100

    Transmit no vat for articles to payone when the order is tax free
    Squashed by @samuelvogel with "Fix wrong computation of tax free flag" from @stefanheppenheimer

--- Components/Classes/PayoneParamBuilder.php
+++ Components/Classes/PayoneParamBuilder.php
@@ -571,10 +571,16 @@ class Mopt_PayoneParamBuilder
    *
    * @param array $basket
    * @param array $shipment
+   * @param array $userData
    * @return \Payone_Api_Request_Parameter_Invoicing_Transaction 
    */
-  public function getInvoicing($basket, $shipment)
+  public function getInvoicing($basket, $shipment, $userData)
   {
+    $taxFree = false;
+    if (isset($userData['additional']['charge_vat'])) {
+      $taxFree = ! $userData['additional']['charge_vat'];
+    }
+
     $transaction = new Payone_Api_Request_Parameter_Invoicing_Transaction($params);
 
     foreach ($basket['content'] as $article)
@@ -585,7 +591,7 @@ class Mopt_PayoneParamBuilder
       $params['pr'] = $article['priceNumeric']; //price
       $params['no'] = $article['quantity']; // ordered quantity
       $params['de'] = substr($article['articlename'], 0, 100); // description
-      $params['va'] = number_format($article['tax_rate'], 0, '.', ''); // vat
+      $params['va'] = $taxFree ? 0 : number_format($article['tax_rate'], 0, '.', ''); // vat
       $params['it'] = Payone_Api_Enum_InvoicingItemType::GOODS; //item type
       if ($article['modus'] == 2)
       {
@@ -608,7 +614,7 @@ class Mopt_PayoneParamBuilder
     $params['pr'] = $basket['sShippingcosts']; //price
     $params['no'] = 1; // ordered quantity
     $params['de'] = substr($shipment['name'], 0, 100); // description check length
-    $params['va'] = number_format($basket['sShippingcostsTax'], 0, '.', ''); // vat
+    $params['va'] = $taxFree ? 0 : number_format($basket['sShippingcostsTax'], 0, '.', ''); // vat
     $params['it'] = Payone_Api_Enum_InvoicingItemType::SHIPMENT;
 
     $params = array_map('utf8_encode', $params);
@@ -664,7 +670,11 @@ class Mopt_PayoneParamBuilder
       }
       $params['no']   = $position->getQuantity(); // ordered quantity
       $params['de']   = substr($position->getArticleName(), 0, 100); // description
-      if ($position->getTaxRate() == 0)
+      if ($order->getTaxFree())
+      {
+        $params['va'] = 0;
+      }
+      else if ($position->getTaxRate() == 0)
       {
         $params['va'] = number_format($position->getTax()->getTax(), 0, '.', '');
       }
